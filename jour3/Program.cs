6baebb389802fd110272aabb3b38fd4404dff152
superjekk <superjekk@outlook.com>
2024-12-03 22:28:32 -0500

    FCT : Partie 2 du jour 3
    Trouver le problème a particulièrement été sophistiqué.

--- jour3/Program.cs
+++ jour3/Program.cs
@@ -4,6 +4,19 @@ namespace jour3
 {
     internal class Program
     {
+        static void Application(string ligne, ref long resultat)
+        {
+            var value = Regex.Matches(ligne, @"mul\(\d{1,3},\d{1,3}\)");
+            for (int i = 0; i < value.Count; ++i)
+            {
+                string stringValue = value[i].Value;
+                string value1 = stringValue.Substring(stringValue.IndexOf('(') + 1, stringValue.IndexOf(',') - stringValue.IndexOf('(') - 1);
+                string value2 = stringValue.Substring(stringValue.IndexOf(',') + 1, stringValue.IndexOf(')') - stringValue.IndexOf(',') - 1);
+
+                resultat += long.Parse(value1) * long.Parse(value2);
+            }
+        }
+
         static List<string> ReadFile()
         {
             List<string> lignes = new();
@@ -22,18 +35,44 @@ namespace jour3
 
             foreach (var ligne in donnees)
             {
-                var value = Regex.Matches(ligne, @"mul\(\d{1,3},\d{1,3}\)");
-                for (int i = 0; i < value.Count; ++i)
-                {
-                    string stringValue = value[i].Value;
-                    string value1 = stringValue.Substring(stringValue.IndexOf('(') + 1, stringValue.IndexOf(',') - stringValue.IndexOf('(') - 1);
-                    string value2 = stringValue.Substring(stringValue.IndexOf(',') + 1, stringValue.IndexOf(')') - stringValue.IndexOf(',') - 1);
-
-                    resultat += long.Parse(value1) * long.Parse(value2);
-                }
+                Application(ligne, ref resultat);
             }
 
             Console.WriteLine(resultat);
+
+            string exemple = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+            exemple = Regex.Replace(exemple, @"don\'t\(\).*?do\(\)", "");
+            Console.WriteLine(exemple);
+
+            long resultat2 = 0L;
+
+            string grosseLigne = "";//string.Join("", donnees);
+            bool lireDebut = true;
+            for (int i = 0; i < donnees.Count; ++i)
+            {
+                if (!lireDebut)
+                {
+                    donnees[i] = donnees[i].Remove(0, donnees[i].IndexOf("do()"));
+                }
+
+                MatchCollection collectionDo = Regex.Matches(donnees[i], "do\\(\\)");
+                MatchCollection collectionDont = Regex.Matches(donnees[i], "don\\'t\\(\\)");
+                lireDebut = collectionDo.Last().Index > collectionDont.Last().Index;
+                if (!lireDebut)
+                {
+                    donnees[i] = donnees[i].Remove(collectionDont.Last().Index);
+                }
+                //donnees[i] = Regex.Replace(donnees[i], @"don't\(\).*?do\(\)", "");
+
+                //Application(donnees[i], ref resultat2);
+            }
+            grosseLigne = string.Join(null, donnees);
+            Console.WriteLine(grosseLigne);
+            grosseLigne = Regex.Replace(grosseLigne, @"don't\(\).*?do\(\)", "do()");
+            Console.WriteLine(grosseLigne);
+            Application(grosseLigne, ref resultat2);
+
+            Console.WriteLine(resultat2);
         }
     }
 }
